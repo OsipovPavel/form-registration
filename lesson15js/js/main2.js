4750e421cd8b6c999212876b4d70f1b8eeb7ca93
Pavel <85952748+Polachello@users.noreply.github.com>
2021-10-19 21:20:21 +0300

    Add files via upload

---
+++ lesson15js/js/main2.js
@@ -0,0 +1,11 @@
+'use strict';
+
+let formRegistration = document.querySelector('#form-registration');
+let email = document.querySelector('#email');
+let password = document.querySelector('#password');
+let repeatPassword = document.querySelector('#repeat-password');
+
+formRegistration.addEventListener('submit', event => {
+    event.preventDefault();
+    
+})
